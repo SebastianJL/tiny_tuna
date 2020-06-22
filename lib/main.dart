01379069b3428a1f9bea8981fa77cbfc2a6f73bd
SebastianJL <johannes.lade@yahoo.com>
2020-06-22 23:33:52 +0200

    Move FFT into HompageBloc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:math';
-
 import 'package:charts_flutter/flutter.dart';
-import 'package:fft/fft.dart';
 import 'package:flutter/material.dart';
-import 'package:my_complex/my_complex.dart';
-import 'package:supercharged/supercharged.dart';
 import 'package:tinytuna/src/homepage_bloc.dart';
 import 'package:tinytuna/src/button_event.dart';
 
@@ -29,32 +24,17 @@ class HomePage extends StatelessWidget {
   final HomepageBloc bloc = HomepageBloc();
   final String title;
 
-    Widget _buildBarChart(
-      BuildContext context, AsyncSnapshot<List<int>> snapshot) {
-    List<num> data;
-    Window window;
-    List<Complex> transformed;
+  Widget _buildBarChart(
+      BuildContext context, AsyncSnapshot<List<num>> snapshot) {
     Series<num, String> series;
 
     if (!snapshot.hasData) {
       return Text('No data yet.');
     }
-    data = snapshot.data.toList();
-    num m = data.averageBy((n) => n);
-    data = data.map((e) => e - m).toList();
-    int closestPowerOf2 = pow(2, log(data.length) ~/ log(2));
-    data = data.sublist(0, closestPowerOf2);
 
-    window = Window(WindowType.HAMMING);
-    data = window.apply(data);
-//    if (_frequencyMode) {
-//      transformed = FFT().Transform(data);
-//      data = transformed.map((e) => e.modulus).toList();
-//    }
-    data = data.chunked(32).map((e) => e.averageBy((n) => n)).toList();
     series = Series(
       id: 'bla',
-      data: data,
+      data: snapshot.data,
       measureFn: (datum, index) => datum,
       domainFn: (datum, index) => index.toString(),
     );
@@ -78,21 +58,21 @@ class HomePage extends StatelessWidget {
           ButtonBar(
             children: <Widget>[
               StreamBuilder<Object>(
-                stream: bloc.frequencyModeStateStream,
-                initialData: false,
-                builder: (context, snapshot) {
-                  return Switch(
-                    value: snapshot.data,
-                    onChanged: (bool newValue)=> bloc.buttonEventSink.add(ButtonEvent.FrequencyModeButtonPressed),
-                  );
-                }
-              ),
+                  stream: bloc.frequencyModeStateStream,
+                  initialData: false,
+                  builder: (context, snapshot) {
+                    return Switch(
+                      value: snapshot.data,
+                      onChanged: (bool newValue) => bloc.buttonEventSink
+                          .add(ButtonEvent.FrequencyModeButtonPressed),
+                    );
+                  }),
             ],
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.fromLTRB(8, 8, 8, 80),
-              child: StreamBuilder<List<int>>(
+              child: StreamBuilder<List<num>>(
                 stream: bloc.audioDataStream,
                 builder: _buildBarChart,
               ),
@@ -101,17 +81,17 @@ class HomePage extends StatelessWidget {
         ],
       ),
       floatingActionButton: StreamBuilder<bool>(
-        stream: bloc.audioStateStream,
-        initialData: false,
-        builder: (context, snapshot) {
-          return FloatingActionButton(
-            onPressed: () => bloc.buttonEventSink.add(ButtonEvent.AudioButtonPressed),
-            tooltip: 'Listen',
-            backgroundColor: snapshot.data ? Colors.red : Colors.blue,
-            child: snapshot.data ? Icon(Icons.stop) : Text('Listen'),
-          );
-        }
-      ),
+          stream: bloc.audioStateStream,
+          initialData: false,
+          builder: (context, snapshot) {
+            return FloatingActionButton(
+              onPressed: () =>
+                  bloc.buttonEventSink.add(ButtonEvent.AudioButtonPressed),
+              tooltip: 'Listen',
+              backgroundColor: snapshot.data ? Colors.red : Colors.blue,
+              child: snapshot.data ? Icon(Icons.stop) : Text('Listen'),
+            );
+          }),
     );
   }
 }
--- lib/src/homepage_bloc.dart
+++ lib/src/homepage_bloc.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:math';
 
+import 'package:fft/fft.dart';
 import 'package:mic_stream/mic_stream.dart';
+import 'package:my_complex/my_complex.dart';
+import 'package:supercharged/supercharged.dart';
 import 'package:tinytuna/src/button_event.dart';
 
 class HomepageBloc {
@@ -12,7 +16,7 @@ class HomepageBloc {
   final _audioStateController = StreamController<bool>();
   final _frequencyModeStateController = StreamController<bool>();
   final Stream<List<int>> _microphone = microphone();
-  final _audioDataController = StreamController<List<int>>();
+  final _audioDataController = StreamController<List<num>>();
 
   StreamSink<ButtonEvent> get buttonEventSink => _buttonEventController.sink;
 
@@ -22,13 +26,15 @@ class HomepageBloc {
 
   StreamSink<bool> get _audioStateSink => _audioStateController.sink;
 
-  Stream<bool> get frequencyModeStateStream => _frequencyModeStateController.stream;
+  Stream<bool> get frequencyModeStateStream =>
+      _frequencyModeStateController.stream;
 
-  StreamSink<bool> get _frequencyModeStateSink => _frequencyModeStateController.sink;
+  StreamSink<bool> get _frequencyModeStateSink =>
+      _frequencyModeStateController.sink;
 
-  Stream<List<int>> get audioDataStream => _audioDataController.stream;
+  Stream<List<num>> get audioDataStream => _audioDataController.stream;
 
-  StreamSink<List<int>> get _audioDataSink => _audioDataController.sink;
+  StreamSink<List<num>> get _audioDataSink => _audioDataController.sink;
 
   HomepageBloc() {
     _buttonEventStream.listen(_mapEventToState);
@@ -39,8 +45,7 @@ class HomepageBloc {
       case ButtonEvent.AudioButtonPressed:
         {
           if (_listener == null) {
-            _listener =
-                _microphone.listen((event) => _audioDataSink.add(event));
+            _listener = _microphone.listen((event) => _processMicrophoneEvent(event));
           } else if (_listening) {
             _listener.pause();
           } else {
@@ -58,6 +63,28 @@ class HomepageBloc {
           break;
         }
     }
+  }
+
+  _processMicrophoneEvent(List<int> event) {
+    List<num> data;
+    Window window;
+    List<Complex> transformed;
+
+    data = event;
+    num m = data.averageBy((n) => n);
+    data = data.map((e) => e - m).toList();
+    int closestPowerOf2 = pow(2, log(data.length) ~/ log(2));
+    data = data.sublist(0, closestPowerOf2);
+
+    window = Window(WindowType.HAMMING);
+    data = window.apply(data);
+    if (_frequencyMode) {
+      transformed = FFT().Transform(data);
+      data = transformed.map((e) => e.modulus).toList();
+    }
+    data = data.chunked(32).map((e) => e.averageBy((n) => n)).toList();
+
+    _audioDataSink.add(data);
   }
 
   void close() {
